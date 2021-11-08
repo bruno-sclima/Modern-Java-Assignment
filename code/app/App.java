fa1eb5dd800cb9b6a569c986a020e6b1521dbd2e
Bruno Silveira Cequeira Lima <brunobhsclima@gmail.com>
2021-11-08 18:31:00 -0300

    Update App.java

--- code/app/App.java
+++ code/app/App.java
@@ -1,1 +1,42 @@
+package app;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.*;
+
+
+import business.*;
+public class App {
+
+	public static void main(String[] args) {
+		List<Block> blockchain = new ArrayList<>();
+		var block = new Block("first block", "", new Date().getTime());
+		for(var i=0;i<5;i++) {
+			var miner = new Miners(block,blockchain);
+			ExecutorService exe = Executors.newFixedThreadPool(3);
+			try {
+			
+			exe.execute(miner);
+			exe.execute(miner);
+			exe.execute(miner);
+			exe.shutdown();
+			
+				exe.awaitTermination(Long.MAX_VALUE, TimeUnit.MILLISECONDS);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} finally {
+				exe.shutdownNow();
+				block= miner.newBlock();
+			}
+		}
+		System.out.println("Final result of the blockchain:");
+		blockchain.forEach(b ->System.out.println(b.toString()));
+		
+		
+		
+	}
+
+	
+
+}
 
